d8f15e2876dd49be1916d1b4a52e7753fdf7d06e
arondasamuel123 <arondasamuel123@gmail.com>
2020-02-29 10:18:20 +0300

    Created models for the gallery app with the necessary constraints

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+    
+class Location(models.Model):
+    loction = models.CharField(max_length=20)
