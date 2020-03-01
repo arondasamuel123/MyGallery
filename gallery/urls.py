39764c693bf1bd530f7ed62b3dfe42235f13a0d1
arondasamuel123 <arondasamuel123@gmail.com>
2020-03-01 19:42:02 +0300

    Created url path to display specific image

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,7 +7,8 @@ from .import views
 urlpatterns  = [
     path('', views.index, name='index'),
     path('search/', views.search_images, name='search_images'),
-    path('filter/<int:img_location>', views.filter_by_loc, name='filter_loc')
+    path('filter/<int:img_location>', views.filter_by_loc, name='filter_loc'),
+    path('image/<int:id>', views.specific_image, name='specific_image')
 ]
 
 if settings.DEBUG:
